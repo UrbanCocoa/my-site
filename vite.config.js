a4f2174bc457036a73d179f926e21ee81a831f5e
UrbanCocoa <109919497+UrbanCocoa@users.noreply.github.com>
2025-10-08 16:02:16 -0700

    yes

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/', // 👈 THIS IS THE IMPORTANT FIX
   plugins: [react()],
   server: {
-    hmr: false, // disables hot module replacement to avoid eval
+    hmr: false,
   },
   esbuild: {
-    legalComments: 'none', // ensures no eval for dev sourcemaps
+    legalComments: 'none',
   },
 });
